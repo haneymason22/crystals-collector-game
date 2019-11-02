7d3e1c26ea5c7c59aea7f8f068886aa80b9b508e
MHane952 <haneymason22@gmail.com>
2019-11-01 22:48:51 -0500

    Javascript functioning properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,78 @@ $(document).ready(function() {
     
     var wins = 0;
     var losses = 0;
+    var totalScore = 0;
+    $("#total-score").html(totalScore)
 
-    
-}
+    function win () {
+        wins ++;
+        $("#wins").html("Wins: " + wins)
+        reset();
+    }
+
+    function lose () {
+        losses++;
+        $("#losses").html("Losses: " + losses)
+        reset();
+    }
+
+    function reset() {
+        totalScore = 0;
+        $('#total-score').text(totalScore);
+        randomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+        $("#random-number").html(randomNumber);
+        crystal1 = Math.floor(Math.random() * 12) + 1;
+        crystal2 = Math.floor(Math.random() * 12) + 1;
+        crystal3 = Math.floor(Math.random() * 12) + 1;
+        crystal4 = Math.floor(Math.random() * 12) + 1;
+    };
+
+    $("#first-crystal").on("click", function () {
+        totalScore = totalScore + crystal1
+        $("#total-score").html(totalScore)
+        if (totalScore === randomNumber) {
+            win();
+            reset();
+        } else if (totalScore > randomNumber) {
+            lose();
+            reset();
+        };
+    });
+
+    $("#second-crystal").on("click", function () {
+        totalScore = totalScore + crystal2
+        $("#total-score").html(totalScore)
+        if (totalScore === randomNumber) {
+            win();
+            reset();
+        } else if (totalScore > randomNumber) {
+            lose();
+            reset();
+        };
+    });
+
+    $("#third-crystal").on("click", function () {
+        totalScore = totalScore + crystal3
+        $("#total-score").html(totalScore)
+        if (totalScore === randomNumber) {
+            win();
+            reset();
+        } else if (totalScore > randomNumber) {
+            lose();
+            reset();
+        };
+    });
+
+    $("#fourth-crystal").on("click", function () {
+        totalScore = totalScore + crystal4
+        $("#total-score").html(totalScore)
+        if (totalScore === randomNumber) {
+            win();
+            reset();
+        } else if (totalScore > randomNumber) {
+            lose();
+            reset();
+        };
+    });
+
+});
