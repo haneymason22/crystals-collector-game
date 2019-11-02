9716f755d103ee31628cb53fafd7d3c4e9feb94e
MHane952 <haneymason22@gmail.com>
2019-11-01 22:16:44 -0500

    Initialized all aspects but javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    var randomNumber = Math.floor(Math.random() * ((120-19) + 1) + 19);
+    $("#random-number").html(randomNumber);
+    
+    var crystal1 = Math.floor(Math.random() * 12) + 1;
+    var crystal2 = Math.floor(Math.random() * 12) + 1;
+    var crystal3 = Math.floor(Math.random() * 12) + 1;
+    var crystal4 = Math.floor(Math.random() * 12) + 1;
+    
+    var wins = 0;
+    var losses = 0;
+
+    
+}
